d5d94d81d41f5efa5e403c26aebef5b1cbbdfb90
Jonathan Benicio de Lima Oliveira <45011024+JonathanBenicio@users.noreply.github.com>
2021-12-15 19:09:57 -0300

    acabo

--- Livraria/src/BLL/AuthorBLL.java
+++ Livraria/src/BLL/AuthorBLL.java
@@ -41,4 +41,89 @@ public class AuthorBLL {
         int idAuthor = Integer.parseInt(new Scanner(System.in).nextLine());
         this.author = AuthorDao.consultar(idAuthor);
     }
+
+    public void gerenciar() {
+        boolean repite = true;
+        while (repite) {
+
+            System.out.println("---------------------------------");
+            System.out.println("Escolha qual ação deseja realizar: ");
+            System.out.println("Opção | Ação");
+            System.out.println("  1     nova Autor");
+            if (AuthorDao.listar().size() > 0) {
+
+                System.out.println("  2     visualizar Autor");
+                System.out.println("  3     alterar nomev Autor");
+                System.out.println("  4     remover Autor");
+            }
+            System.out.println("  5     VOLTAR");
+
+            switch (new Scanner(System.in).nextLine()) {
+                case "1":
+                    System.out.println("Criando Ordem");
+                    criaAuthor();
+                    break;
+
+                case "2":
+                    listAuthor();
+
+                    break;
+
+                case "3":
+                    alterarAuthor();
+
+                    break;
+
+                case "4":
+                    excluirAuthor();
+
+                    break;
+
+                case "5":
+                    repite = false;
+                    break;
+
+                default:
+
+                    System.out.println("Opção incorreta");
+                    break;
+            }
+        }
+    }
+
+    private void excluirAuthor() {
+        listAuthor();
+        System.out.println("--------------------------------");
+        System.out.println("Seleciona o Id do livro que deseja remover:");
+        selecionarAuthor();
+        if (AuthorDao.delete(author.getId())) {
+
+            System.out.println("Autor removida com sucesso");
+
+            listAuthor();
+        }
+    }
+
+    private void alterarAuthor() {
+        
+        selecionarAuthor();
+
+        System.out.println("--------------------------------");
+        System.out.println("Informe o Nome do Autor:");
+        author.setNome(new Scanner(System.in).nextLine());
+    }
+
+    private void criaAuthor() {
+        author = new Author();
+
+        System.out.println("--------------------------------");
+        System.out.println("Informe o Nome do Autor:");
+        author.setNome(new Scanner(System.in).nextLine());
+
+
+        if (AuthorDao.insert(author) > 0) {
+
+            System.out.println("Autor Cadastrado com sucesso");
+        }
+    }
 }
--- Livraria/src/Dao/PublisherDao.java
+++ Livraria/src/Dao/PublisherDao.java
@@ -26,23 +26,90 @@ import Model.Publisher;
  */
 public class PublisherDao {
 
+    // public static Publisher consultar(int id) {
+    //     Connection conn = UtilsDao.getConnection();
+    //     Publisher publisher = new Publisher();
+
+    //     try {
+    //         String queryAuthor = "Select * from library.publisher " +
+    //                 "where puid = ?";
+
+    //         PreparedStatement prepAuthor = conn.prepareStatement(queryAuthor);
+    //         prepAuthor.setInt(1, id);
+
+    //         ResultSet idAthor = prepAuthor.executeQuery();
+    //         if (idAthor.next()) {
+
+    //             publisher.setId(idAthor.getInt("puid"));
+    //             publisher.setName(idAthor.getString("name"));
+    //             publisher.setEmail(idAthor.getString("email"));
+    //         } else {
+    //             publisher = null;
+    //         }
+    //         conn.close();
+
+    //     } catch (Exception e) {
+    //         // TODO: handle exception
+    //         publisher = null;
+    //     }
+    //     return publisher;
+        
+         // insercao da ordem de compra
+    public static int insert(Publisher publisher) {
+
+        Connection conn = UtilsDao.getConnection();
+        int response = 0;
+
+        try {
+            // faz a insercao da ordem de compra
+            String queryPublisher = "INSERT INTO `library`.`publisher`(`name`,`email`)"
+                    + "VALUES (?,?)";
+
+            PreparedStatement prepPublisher = conn.prepareStatement(queryPublisher, Statement.RETURN_GENERATED_KEYS);
+            prepPublisher.setString(1, publisher.getName());
+            prepPublisher.setString(2, publisher.getEmail());
+
+            prepPublisher.executeUpdate();
+
+            // pega o id da ordem de venda
+            ResultSet idPublisher = prepPublisher.getGeneratedKeys();
+            if (idPublisher.next()) {
+
+                response = idPublisher.getInt(1);
+
+            } else {
+                response = 0;
+            }
+            conn.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            response = 0;
+        }
+        return response;
+
+    }
+
+    // consulta ordem
     public static Publisher consultar(int id) {
         Connection conn = UtilsDao.getConnection();
         Publisher publisher = new Publisher();
 
         try {
-            String queryAuthor = "Select * from library.publisher " +
+            String queryPublisher = "Select * from library.publisher " +
                     "where puid = ?";
 
-            PreparedStatement prepAuthor = conn.prepareStatement(queryAuthor);
-            prepAuthor.setInt(1, id);
+            PreparedStatement prepPublisher = conn.prepareStatement(queryPublisher);
+            prepPublisher.setInt(1, id);
 
-            ResultSet idAthor = prepAuthor.executeQuery();
-            if (idAthor.next()) {
+            ResultSet idPublisher = prepPublisher.executeQuery();
+            if (idPublisher.next()) {
 
-                publisher.setId(idAthor.getInt("puid"));
-                publisher.setName(idAthor.getString("name"));
-                publisher.setEmail(idAthor.getString("email"));
+                publisher.setId(idPublisher.getInt("puid"));
+                
+                publisher.setId(idPublisher.getInt("puid"));
+                publisher.setName(idPublisher.getString("name"));
+                publisher.setEmail(idPublisher.getString("email"));
             } else {
                 publisher = null;
             }
@@ -53,5 +120,84 @@ public class PublisherDao {
             publisher = null;
         }
         return publisher;
+    }
+
+    // atualiza a ordem
+    public static boolean atualizar(Publisher publisher) {
+        Connection conn = UtilsDao.getConnection();
+        boolean response = false;
+
+        try {
+
+            String queryPublisher = "UPDATE library.publisher " +
+                    "SET name = ?, email = ? WHERE puid = ?";
+
+            PreparedStatement prepPublisher = conn.prepareStatement(queryPublisher);
+
+            prepPublisher.setString(1, publisher.getName());
+            prepPublisher.setString(2, publisher.getEmail());
+            prepPublisher.setInt(3, publisher.getId());
+
+            response = prepPublisher.executeUpdate() > 0;
+            conn.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            response = false;
+        }
+
+        return response;
+
+    }
+
+    public static boolean delete(int id) {
+        Connection conn = UtilsDao.getConnection();
+        boolean response = false;
+
+        try {
+            String queryPublisher = "DELETE FROM library.publisher WHERE puid = ?";
+
+            PreparedStatement prepPublisher = conn.prepareStatement(queryPublisher);
+
+            prepPublisher.setInt(1, id);
+
+            response = prepPublisher.executeUpdate() > 0;
+
+            conn.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            response = false;
+        }
+
+        return response;
+    }
+
+    public static List<Publisher> listar() {
+        Connection conn = UtilsDao.getConnection();
+        List<Publisher> list = new ArrayList<Publisher>();
+
+        try {
+            String queryPublisher = "Select * from library.publisher";
+
+            PreparedStatement prepPublisher = conn.prepareStatement(queryPublisher);
+
+            ResultSet idPublisher = prepPublisher.executeQuery();
+            while (idPublisher.next()) {
+                Publisher publisher = new Publisher();
+
+                
+                publisher.setId(idPublisher.getInt("puid"));
+                publisher.setName(idPublisher.getString("name"));
+                publisher.setEmail(idPublisher.getString("email"));
+                list.add(publisher);
+            }
+            conn.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            list = null;
+        }
+        return list;
     }
 }
